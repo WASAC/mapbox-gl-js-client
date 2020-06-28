db2789358e68306e16fdb7413c2436111a8a1bc3
Jin IGARASHI <wushilan0623@gmail.com>
2020-06-28 12:05:27 +0900

    added version for loading stylefiles in order to avoid using cache.

--- src/config.js
+++ src/config.js
@@ -1,9 +1,11 @@
+const version = 0.1;
+
 module.exports = {
     accessToken : 'pk.eyJ1IjoiamluLWlnYXJhc2hpIiwiYSI6ImNrOHV1Nm9mdTAzMGIzdHNmbDBmZzllNnIifQ.J-ZRzlVGLH6Qm2UbCmYWeA',
     attribution : '©WASAC,Ltd.',
     styles : [
-        { title: 'Street', uri: 'https://wasac.github.io/mapbox-stylefiles/street/style.json',}, 
-        { title: 'Satellite', uri: 'https://wasac.github.io/mapbox-stylefiles/satellite/style.json'},
+        { title: 'Street', uri: `https://wasac.github.io/mapbox-stylefiles/street/style.json?version=${version}`,}, 
+        { title: 'Satellite', uri: `https://wasac.github.io/mapbox-stylefiles/satellite/style.json?version=${version}`},
         // { title: 'UN Vector', uri: 'https://wasac.github.io/mapbox-stylefiles/unvt/style.json'}
     ],
     center: [30.0291, -2.0032],
@@ -13,7 +15,7 @@ module.exports = {
         target: ['wss_name', 'district','po_name'],
         format: (p) => {return `${p.wss_id}-${p.wss_name}, ${p.po_name}, ${p.district}`},
         place_type: ['wss'],
-        placeholder: 'Name of WSS, PO, District',
+        placeholder: 'Search WSS/PO/District',
         limit: 10,
         zoom: 13,
     },
